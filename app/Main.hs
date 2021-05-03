7060efccbe2e5c73c432c2069b1c5c07ccd904a6
James Carlson <jxxcarlson@gmail.com>
2021-05-03 18:00:37 -0400

    Complete chapter 2, Parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,10 @@
 module Main where
     
 import System.Environment
-
--- main :: IO ()
--- main = do
---  args <- getArgs
---  let a = read (args !! 0) :: Int
---  let b = read (args !! 1) :: Int
---  let sum = a + b
---  putStrLn ("The sum is " ++ show sum)
+import Parser
 
 main :: IO ()
-main = do
- (arg1:arg2:_) <- getArgs
- let a = read arg1 :: Int
- let b = read arg2 :: Int
- -- let sum = a + b
- putStrLn ("The sum is " ++ (show $ a + b))
+main = do 
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
 
